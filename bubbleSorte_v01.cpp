d89b73dcce4d81b031d9d72c06b5b302abd0f993
Yuri Martins <cyuri.martins@gmail.com>
2018-05-07 01:13:48 -0300

    Create bubbleSorte_v01.cpp

---
+++ bubbleSorte_v01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+//BUBBLE SORT VERSAO 01 (MELHORADO)
+//Yuri Martins
+
+int main(int argc, char** argv) {
+	int x[5], j, i, aux;
+	
+	//CARREGANDO OS NUMEROS NO VETOR	
+	for (i=0; i<=4; i++){
+		cout << "Digite o " << i+1 << " numero: ";
+		cin >> x[i];
+	}
+	
+	//ORDENANDO DE FORMA CRESCENTE LAÇO COM A QUANTIDADE DE ELEMENTOS DO VETOR - 1	
+	for(j=1; j<=4; j++){
+		
+		//LAÇO PERCORRE DA ULTIMA POSICAO A POSIÇÃO J DO VETOR
+		for (i=4; i>=j;i--){
+			if (x[i] < x[i-1]){
+				aux = x[i];
+				x[i] = x[i-1];
+				x[i-1] = aux;
+			}	
+		}
+	}
+	
+	//MOSTRANDO O VETOR ORDENADO
+	
+	for (i=0; i<=4; i++){
+		cout <<i+1<<" numero: " << x[i]<<"\n";
+	}
+	return 0;
+}
